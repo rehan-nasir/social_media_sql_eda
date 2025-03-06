93981dcc514141a1a9443c1803f1119b11854dde
Rehan Nasir <111933400+rehan-nasir@users.noreply.github.com>
2025-03-06 00:38:28 -0500

    Add files via upload

--- Social_Media_EDA.sql
+++ Social_Media_EDA.sql
@@ -1,14 +1,28 @@
 use social_media;
 GO
 
-
-select * from social_media_clean;
-
 -- Converting Age (varchar) to a float, to perform aggregate functions
 alter table social_media_clean
 alter column Age float;
 
+select * from social_media_clean;
+
 -- EDA
+
+-- Average Video and Gaming Time for each Age
+select Age, round(AVG([Daily Video Content Time (hrs)]), 2) as [Average Daily Video Content Time], round(AVG([Daily Gaming Time (hrs)]), 2) [Daily Gaming Time] from social_media_clean
+group by Age
+order by Age desc;
+
+-- Monthly Income and Average Daily Screen Time
+select [Monthly Income (USD)], AVG([Screen Time (hrs)]) as [Average Screen Time] from social_media_clean
+group by [Monthly Income (USD)]
+order by [Monthly Income (USD)] desc;
+
+-- Most popular social media platform
+select [Primary Platform], COUNT(*) as [Num of Users] from social_media_clean
+group by [Primary Platform]
+order by [Num of Users] desc;
 
 -- Total Hours spent daily on each platform
 select [Primary Platform], SUM([Daily Social Media Time (hrs)]) as [Total hrs spent daily] from social_media_clean
@@ -28,15 +42,15 @@ group by [Primary Platform]
 order by [Average age for each platform] desc;
 
 -- Most popular platform in each country
-SELECT Country, [Primary Platform]
-FROM (
-    SELECT Country, [Primary Platform], COUNT(*) AS [Num of users],
-           ROW_NUMBER() OVER (PARTITION BY Country ORDER BY COUNT(*) DESC) AS rn
-    FROM social_media_clean
-    GROUP BY Country, [Primary Platform]
+select Country, [Primary Platform]
+from (
+    select Country, [Primary Platform], COUNT(*) as [Num of users],
+           ROW_NUMBER() over (partition by Country order by COUNT(*) desc) as rn
+    from social_media_clean
+    group by Country, [Primary Platform]
 ) as ranked
-WHERE rn = 1
-ORDER BY Country;
+where rn = 1
+order by Country;
 
 -- Preferred Content Type for each Gender
 select Gender, [Preferred Content Type] 
@@ -50,15 +64,15 @@ order by Gender;
 
 
 -- Most popular platform for each gender
-SELECT Gender, [Primary Platform]
-FROM (
-    SELECT Gender, [Primary Platform], COUNT(*) AS [Num of users],
-           ROW_NUMBER() OVER (PARTITION BY Gender ORDER BY COUNT(*) DESC) AS rn
-    FROM social_media_clean
-    GROUP BY Gender, [Primary Platform]
+select Gender, [Primary Platform]
+from (
+    select Gender, [Primary Platform], COUNT(*) as [Num of users],
+           ROW_NUMBER() over (partition by Gender order by COUNT(*) desc) as rn
+    from social_media_clean
+    group by Gender, [Primary Platform]
 ) as ranked
-WHERE rn = 1
-ORDER BY Gender;
+where rn = 1
+order by Gender;
 
 -- Average Social Media Time per Country
 select AVG([Daily Social Media Time (hrs)]) as [Average Social Media Time (hrs)], Country from social_media_clean
@@ -88,4 +102,7 @@ order by [Screen Time (hrs)] desc;
 select [Monthly Income (USD)] ,[Monthly Expenditure on Entertainment (USD)] from social_media_clean
 order by [Monthly Income (USD)] desc;
 
-
+-- Average tech knowledge for each Age
+select Age, AVG([Tech Savviness Level (scale 1-10)]) as [Average Tech Savviness Level] from social_media_clean
+group by Age
+order by [Average Tech Savviness Level] desc;
